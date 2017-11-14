6ef92748dba05038f048ff9c173fab209496020d
Arjan Visser <arjan.visser@eqnon.com>
2017-11-14 15:11:03 +0100

    schema and seed modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114103900) do
+ActiveRecord::Schema.define(version: 20171114130503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,4 +55,13 @@ ActiveRecord::Schema.define(version: 20171114103900) do
     t.index ["reset_password_token"], name: "index_specialists_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id", using: :btree
+  end
+
+  add_foreign_key "subcategories", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating categories...'
+hair = Category.create!({
+  name: "Hair"
+})
+skin = Category.create!({
+  name: "Skin"
+})
+nails = Category.create!({
+  name: "Nails"
+})
+body = Category.create!({
+  name: "Body"
+})
+puts 'Categories added!'
+
+# puts 'Creating subcategories...'
+
+# puts 'Subcategories added!'
+
+# Subcategory.new(subcategory)
+
+subcategories_hair = ["Hair cut", "Hair removal", "Hair trimming / shaving", "Hair colouring", "Hair styling"]
+subcategories_skin = ["Skin care", "Make-up"]
+subcategories_nails = ["Manicure", "Pedicure"]
+subcategories_body = ["Swedish massage therapy", "Aromatherapy massage", "hot stone massage", "deep tissue massage", "shiatsu massage", "thai massage", "pregnancy massage", "reflexology", "other"]
+
+subcategories_hair.each do |subcategory_hair|
+  subcategory_a = Subcategory.new(name: subcategory_hair)
+  subcategory_a.category = hair
+  subcategory_a.save
+end
+
+subcategories_skin.each do |subcategory_skin|
+  subcategory_a = Subcategory.new(name: subcategory_skin)
+  subcategory_a.category = skin
+  subcategory_a.save
+end
+
+subcategories_nails.each do |subcategory_nails|
+  subcategory_a = Subcategory.new(name: subcategory_nails)
+  subcategory_a.category = nails
+  subcategory_a.save
+end
+
+subcategories_body.each do |subcategory_body|
+  subcategory_a = Subcategory.new(name: subcategory_body)
+  subcategory_a.category = body
+  subcategory_a.save
+end
