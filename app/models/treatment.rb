1c95dff0b417bec403e8838c9f5788c84ea265a9
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-14 17:12:21 +0100

    Update treatment.rb

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,6 +1,6 @@
 class Treatment < ApplicationRecord
   belongs_to :specialist
-  belongs_to :specialist
+  belongs_to :subcategory
   validates :description, presence: true, uniqueness: { case_sensitive: false, scope: :specialist, message: "You already have this treatment" }
   validates :price, presence: true, numericality: true
   validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 5 }
