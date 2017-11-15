2420f8bd0eaea361c8514fa1ca7c551b82640ca9
Arjan Visser <arjan.visser@eqnon.com>
2017-11-15 15:31:45 +0100

    raw sketch index page done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115111458) do
+ActiveRecord::Schema.define(version: 20171115134659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20171115111458) do
     t.string   "address"
     t.integer  "rating"
     t.string   "phone_number"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_specialists_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_specialists_on_reset_password_token", unique: true, using: :btree
   end
