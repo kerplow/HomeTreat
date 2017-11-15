47c6968cc59bf13c7f346a0d48123e7ccf767d97
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-15 17:14:05 +0100

    skip authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_specialist!, only: :home
-  skip_before_action :authenticate_client!, only: :home
-  
+  skip_before_action :authenticate_specialist!, only: [:home, :index, :show]
+  skip_before_action :authenticate_client!, only: [:home, :index, :show]
+
   def home
   end
 end
