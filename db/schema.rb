fb4f16ac8c55906fdfa9e25a35032b2a865e3cab
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-15 12:31:20 +0100

    added bio, rating, address and phone number columns to specialists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114163455) do
+ActiveRecord::Schema.define(version: 20171115111458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(version: 20171114163455) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "photo"
+    t.text     "bio"
+    t.string   "address"
+    t.integer  "rating"
+    t.string   "phone_number"
     t.index ["email"], name: "index_specialists_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_specialists_on_reset_password_token", unique: true, using: :btree
   end
