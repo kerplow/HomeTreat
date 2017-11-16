dbf0b60b02dc8cc7b4e31e126c3426968b8eb38c
Arjan Visser <arjan.visser@eqnon.com>
2017-11-16 18:35:19 +0100

    links in cards updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :specialists, only: [ :index, :show ]
+  get 'clients/booking'
 end
