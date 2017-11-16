04ad8e98a533716f2bdbb1eec79d1eb509962e5c
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-16 18:33:52 +0100

    weird filtering, updated searchbox

--- app/controllers/specialists_controller.rb
+++ app/controllers/specialists_controller.rb
@@ -4,15 +4,23 @@ class SpecialistsController < ApplicationController
 
   def index
     @specialists = Specialist.all
+    params[:price][:min] = 0 if params[:price][:min] == ""
+    params[:price][:max] = 1000 if params[:price][:max] == ""
     if params[:subcategory]
-      subcategory_id_array = Subcategory.where(name: params[:subcategory]).pluck(:id)
+    subcategory_id_array = Subcategory.where(name: params[:subcategory]).pluck(:id)
       @specialists = @specialists.joins(:treatments).where("treatments.subcategory_id IN (?)", subcategory_id_array)
     end
-    # @specialists = @specialists.where(price: params[:price[:min]].to_i..params[:price[:max]].to_i) if params[:price]
-    # if params[:rating]
-    #   ratings = rating_convert(params[:rating])
-    #   @specialists = @specialists.where(rating: ratings )
-    # end
+    @specialists = @specialists.joins(:treatments).where("treatments.price BETWEEN ? AND ?", params[:price][:min], params[:price][:max])
+
+    if params[:rating]
+      @specialists = @specialists.where(rating: params[:rating])
+    end
+
+    # @specialists = Specialist.joins(:treatments, :subcategories).includes(:treatments, :subcategories)
+    # if params[:subcategory]
+    #   subcategory_id_array = Subcategory.where(name: params[:subcategory]).pluck(:id)
+    #   @specialists = @specialists.select { |specialist| specialist.treatments.subcategory}
+
   end
 
   def show
